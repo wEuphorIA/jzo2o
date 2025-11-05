065affbcffffe1db761c239f6ca72b4245c6da1b
Euphoria <1534554535@qq.com>
2025-11-05 17:03:24 +0800

    创建jzo2o-foundations

---
+++ jzo2o-foundations/src/main/java/com/jzo2o/foundations/mapper/ServeMapper.java
@@ -0,0 +1,15 @@
+package com.jzo2o.foundations.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jzo2o.foundations.model.domain.Serve;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-07-03
+ */
+public interface ServeMapper extends BaseMapper<Serve> {
+}
