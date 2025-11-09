20e900630f42f916b71017f5e33abaa7d6fa8751
Euphoria <1534554535@qq.com>
2025-11-09 15:59:28 +0800

    优惠券模块

---
+++ jzo2o-market/src/main/java/com/jzo2o/market/service/impl/CouponServiceImpl.java
@@ -0,0 +1,45 @@
+package com.jzo2o.market.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jzo2o.market.mapper.CouponMapper;
+import com.jzo2o.market.model.domain.Coupon;
+import com.jzo2o.market.service.IActivityService;
+import com.jzo2o.market.service.ICouponService;
+import com.jzo2o.market.service.ICouponUseBackService;
+import com.jzo2o.market.service.ICouponWriteOffService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-09-16
+ */
+@Service
+@Slf4j
+public class CouponServiceImpl extends ServiceImpl<CouponMapper, Coupon> implements ICouponService {
+
+    @Resource(name = "seizeCouponScript")
+    private DefaultRedisScript<String> seizeCouponScript;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private IActivityService activityService;
+
+    @Resource
+    private ICouponUseBackService couponUseBackService;
+
+    @Resource
+    private ICouponWriteOffService couponWriteOffService;
+
+
+}
